38327f42c064663139bcca4efed5633324eb6a76
Hanji Gu <hg1154@nyu.edu>
2022-04-19 20:38:37 -0400

    update PairTrade.h

--- Team_Project/Team_Project/main.cpp
+++ Team_Project/Team_Project/main.cpp
@@ -6,11 +6,11 @@
 //
 
 #include <iostream>
-
-using namespace std;
+#include "PairTrade.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
